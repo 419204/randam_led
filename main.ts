0648f5496e9ead8378e49639edba3f62a647b9bb
419204 <115142418+419204@users.noreply.github.com>
2022-12-08 16:47:15 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,39 @@
+function 燈數 (num: number) {
+    if (0 == 0) {
+    	
+    } else {
+    	
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    list = []
+    for (let index = 0; index < 25; index++) {
+        list.push(_0)
+    }
+    n = 0
+    while (n < 3) {
+        a = randint(0, 24)
+        if (list[a] == 0) {
+            list[a] = _1
+            n += 1
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (input.buttonIsPressed(Button.B)) {
+        B次數 += 1
+    } else {
+    	
+    }
+})
+let a = 0
+let n = 0
+let list: number[] = []
+let _1 = 0
+let _0 = 0
+_1 = 1
+_0 = 0
+let B次數 = 0
+basic.forever(function () {
+	
+})
